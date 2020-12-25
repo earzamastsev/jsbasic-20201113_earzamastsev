4b15cbb953c6a6573f225f760d29c4777f9292ec
Eugene Arzamastsev <earzamastsev@gmail.com>
2020-12-25 18:03:50 +0500

    Finish 8-module-1-task

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -3,6 +3,10 @@ import createElement from '../../assets/lib/create-element.js';
 export default class CartIcon {
   constructor() {
     this.render();
+    this.initialTopCoord = 0;
+
+    this.startCartX = this.elem.getBoundingClientRect().x;
+    this.startCartY = this.elem.getBoundingClientRect().y;
 
     this.addEventListeners();
   }
@@ -39,6 +43,62 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    let leftOffset = 20;
+    let topOffset = 50;
+    let rightOffset = 10;
+    let leftElem = document.querySelector('.container');
+    let leftShift = leftElem.getBoundingClientRect().right + leftOffset;
+    let rightShift = document.documentElement.clientWidth - this.elem.offsetWidth - rightOffset;
+
+    let leftIndent = Math.min(leftShift, rightShift) + 'px';
+    let isMobile = document.documentElement.clientWidth <= 767;
+    if (this.initialTopCoord == 0) this.initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    
+    if (isMobile) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    } else {
+    if (window.pageYOffset > this.initialTopCoord) {
+      Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1e3,
+        right: '10px',
+        left: leftIndent
+      });
+    } else {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    }
+  }
+
+    // let pageWidth = document.documentElement.clientWidth;
+    // let pageHeight = document.documentElement.clientHeight;
+
+    // let currentY = this.elem.getBoundingClientRect().y;
+    // let currentX = this.elem.getBoundingClientRect().x;
+    // let width = this.elem.getBoundingClientRect().width;
+    // let height = this.elem.getBoundingClientRect().height;
+
+    // let leftOffset = 20;
+    // let topOffset = 50;
+    // let rightOffset = 10;
+    // let leftElem = document.querySelector('.container');
+    // let leftShift = leftElem.getBoundingClientRect().x + leftElem.getBoundingClientRect().width + leftOffset;
+
+    // if (currentY < 0 || (currentX + width) > pageWidth) {
+    //   this.elem.style.cssText = "position:fixed";
+    //   this.elem.style.top = topOffset + 'px';
+    //   this.elem.style.left =  leftShift + 'px';
+      
+    // }
   }
 }
